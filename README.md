ecaec2cf4baccd4d6d08d47fd16f93cff2b54fff
Ruaidhri McDonnell <ruaidhri.mcdonnell@gmail.com>
2018-07-17 14:59:42 +0100

    migrate code from existing project

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # caching-core-autofac
-For caching related autofac integrations
+
+Out of the box, autofac doesnt provide a way to resolve a way to create an instance of a closed generic from an open generic registration, which is problemattic for our strongly typed caching solution.
+This package allows this behaviour
+
+#### Further reading
+
+* Please refer to [eshopworld.caching.core](https://github.com/eShopWorld/caching-core) for framework details
+
+### IoC container registration
+
+```c#
+public class CacheModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        // default resolution       ICache<T> -> MemoryCache<T>
+        // will resolve a closed generic i.e. ICache<Dog> to a MemoryCache<Dog>, using the factory method of the MemoryCacheFactory
+        builder.RegisterSource(new CacheRegistrationSource<MemoryCacheFactory>(typeof(ICache<>)));
+    }
+}
+```
---
+++ src/Eshopworld.Caching.Core.Autofac/CacheRegistrationSource.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Autofac;
+using Autofac.Core;
+using Autofac.Core.Activators.Delegate;
+using Autofac.Core.Lifetime;
+using Autofac.Core.Registration;
+
+namespace Eshopworld.Caching.Core.Autofac
+{
+    // dear sonar - i hate having to do this to stop you whining
+    public class CacheRegistrationSource
+    {
+        protected CacheRegistrationSource() { } // dear sonar - i hate having to do this to stop you whining
+        protected static readonly MethodInfo OpenGenericFactoryMethod = typeof(ICacheFactory).GetRuntimeMethod(nameof(ICacheFactory.CreateDefault),new Type[0]);
+    }
+
+    public class CacheRegistrationSource<TCFactory> : CacheRegistrationSource, IRegistrationSource where TCFactory : ICacheFactory
+    {
+        private readonly Type cacheOpenGenericType;
+
+        public bool IsAdapterForIndividualComponents => false;
+
+        public CacheRegistrationSource(Type cacheType)
+        {
+            cacheOpenGenericType = cacheType;
+        }
+
+        public IEnumerable<IComponentRegistration> RegistrationsFor(Service service, Func<Service, IEnumerable<IComponentRegistration>> registrationAccessor)
+        {
+            if (!(service is IServiceWithType swt) || !swt.ServiceType.IsConstructedGenericType || swt.ServiceType.GetGenericTypeDefinition() != cacheOpenGenericType) yield break;
+
+            var registration = new ComponentRegistration(Guid.NewGuid(),new DelegateActivator(swt.ServiceType, (c, p) =>
+                {
+                    // factory method is generic, so we have to use reflection to make the call.
+                    return OpenGenericFactoryMethod.MakeGenericMethod(swt.ServiceType.GenericTypeArguments)
+                        .Invoke(c.Resolve<TCFactory>(), null);
+
+                }), new CurrentScopeLifetime(), InstanceSharing.None, InstanceOwnership.OwnedByLifetimeScope,
+                new[] { service }, new Dictionary<string, object>());
+
+            yield return registration;
+        }
+    }
+}
